713a25211b71c86aac9137d7029a39e4c0fc07cf
Redmond Chan <redmondchan92@gmail.com>
2019-01-06 20:55:08 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -19,7 +19,15 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season, array|
+    array.each do |describe|
+      if describe["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
+  count# code here
 end
 
 def get_occupation(data, hometown)
