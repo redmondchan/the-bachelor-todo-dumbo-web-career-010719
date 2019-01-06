bbe17800b1cc62cf574720cc84d8622bbb663337
Redmond Chan <redmondchan92@gmail.com>
2019-01-06 20:48:30 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,9 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |array|
+    if array["status"] == "Winner"
+      return array["name"].split("").first
+    end
+  end# code here
 end
 
 def get_contestant_name(data, occupation)
